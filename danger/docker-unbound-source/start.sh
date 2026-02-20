bf94f33d236c5aa8a1199f611526010553abdd6e
Manoj Menon <manojmenon.menon@gmail.com>
2026-02-20 15:52:15 +0000

    Cleaned upo the directory

--- danger/docker-unbound-source/start.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-# thank you to https://github.com/mjhwully for cap-add trick : https://github.com/MatthewVance/unbound-docker-rpi/issues/4
-sudo podman      run                                          \
-                --rm                                     \
-                --network host                           \
-                --cap-add NET_ADMIN                      \
-                --volume ./conf:/conf                    \
-                docker.io/homeserversample/unbound       \
-                    unbound -d -c /conf/pi-hole.conf
